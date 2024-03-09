71bf1588bb4afdcbfa903dff63874dde742f491c
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-10 00:21:27 +0530

    check positive number is divisible by 5 and 3

---
+++ CONDITIONAL QUESTION/enteredPositiveNumberIsDivisibleByFiveOrThree.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number ";
+    cin>>num;
+
+    if(num%5==0){
+        cout<<"this number is divisible by five";
+    }
+
+    else if(num%3==0){
+        cout<<"this number is divisible by three";
+    }
+    else{
+        cout<<"this number nither divisible by five nor three";
+    }
+}
