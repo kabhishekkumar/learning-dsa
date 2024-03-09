013cb32e40fb3a632b4efd5630157241f19e8a46
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-09 11:19:10 +0530

    code is wrong

---
+++ PRACTISE QUESTIONS/preTheOutput2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+float a = 5, b = 2; 
+int c; 
+c = a % b; 
+cout<<c;
+return 0;
+}
+
+//code error 
+//if we change float datatype into integer then this code is run easily
+
