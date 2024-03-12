8b07d589f091a91fd00007c68dd60cb4473f37ad
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-12 14:18:17 +0530

    print the any number table which you want you

---
+++ BASIC PROBLEMS OF LOOPS/printTheTableOfN.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,table;
+    cout<<"enter the value of n = ";
+    cin>>n;
+    for(i=1; i<=10; i++){
+table = i*n;
+cout<<"print the table = "<<table<<endl;
+    }
+
+}
