4ad66bf750ce8a4f5d140ef5ddd1876584cb60a4
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-12 14:07:22 +0530

    print number which are divided three

---
+++ BASIC PROBLEMS OF LOOPS/printAllNumberBetweenOneToHundradWhichAreDividedByThree.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=1; i<=100; i++){
+        if(i%3==0){
+            cout<<"this number is divisible by three = "<<i<<endl;
+        }
+    }
+}
