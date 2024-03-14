b27f69ecc18f6ccb1cc3c0376c0bcfffebaf89fd
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-14 14:23:28 +0530

    print sum of all digit even number

---
+++ PROBLEMS ON LOOP 14 MARCH/writeAProgreamTOPrintAllEvenDigitNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout<<"print the size of  long = "<<sizeof(number)<<endl;
+
+    // Make sure the number is positive
+    number = abs(number);
+
+    int sum = 0;
+    while (number > 0) {
+        int digit = number % 10;
+        if (digit % 2 == 0) {
+            sum += digit;
+        }
+        number /= 10;
+    }
+
+    cout << "Sum of even digits: " << sum << std::endl;
+
+    return 0;
+}
