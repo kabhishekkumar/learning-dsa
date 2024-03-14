58903dd43f1e391cf775cd97e79d82143ee6840c
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-14 12:56:58 +0530

    this code is wrong

---
+++ PROBLEMS ON LOOP 14 MARCH/predictOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+}
+
+//we are not declare data type a and b and also not declare value of a and b 
+//we are not inislige value i and decrement or increment value of a and b
