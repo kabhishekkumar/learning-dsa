37164d59209fa87bac428b99f4b08bdeca708d48
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-14 13:14:03 +0530

    this code is wrong

---
+++ PROBLEMS ON LOOP 14 MARCH/predictOutput2.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl;
+}
+
+//in this code condition is wrong and we are not declare increment and decrement operators
