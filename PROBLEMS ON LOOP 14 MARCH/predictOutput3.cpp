48deb3e50a3e5c77c6b20306280a4672d95e0311
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-14 13:41:27 +0530

    this code is run 135 time

--- PROBLEMS ON LOOP 14 MARCH/predictOutput3.cpp
+++ PROBLEMS ON LOOP 14 MARCH/predictOutput3.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main( ) {
-int t = 10;
-while (t/=2) {
-cout << "Hello" << endl;
+float t = 10;
+while (t /= 2) {
+cout << "Hello" <<t<< endl;
 }
 }
