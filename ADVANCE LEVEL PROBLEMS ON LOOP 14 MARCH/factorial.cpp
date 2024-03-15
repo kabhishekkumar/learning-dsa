9cf2efd886958d6ed877f4f6baeef95d9ab81e24
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-15 16:03:14 +0530

    print factorial

---
+++ ADVANCE LEVEL PROBLEMS ON LOOP 14 MARCH/factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i,n,fact=0;
+  fact =1;
+  cout<<"enter the n number = ";
+  cin>>n;
+
+  for(i = 1; i <= n; i++){
+    fact = fact *i;
+    cout<<fact<<", ";
+  }
+}
