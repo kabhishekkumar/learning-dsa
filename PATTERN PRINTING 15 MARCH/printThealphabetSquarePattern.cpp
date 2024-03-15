833bb752d14d13b07dc489210900c9d9e5ecb3d2
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-15 17:07:53 +0530

    print the alphabet square pattern

---
+++ PATTERN PRINTING 15 MARCH/printThealphabetSquarePattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+char  ch;
+char cp;
+
+    for(ch='A'; ch<='D'; ch++){
+        for(cp='A'; cp<='D'; cp++){
+            cout<<cp;
+        }
+        cout<<endl;
+    }
+}
