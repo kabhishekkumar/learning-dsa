7db7fbb5fca993f05ec0784b854704d450dad6c4
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-15 16:31:34 +0530

    print the star reverse tringle

---
+++ PATTERN PRINTING 15 MARCH/printTheStarReverseTringle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter the n number = ";
+    cin>>n;
+
+
+    for(i=1; i<=n; i++){
+        for(j=i; j<=n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
