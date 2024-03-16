4527645e00212fc5177e7d615c1eedfb9583abc3
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-16 14:48:02 +0530

    print the number flipped tringle pattern

---
+++ PATTERN PRINTING 16 MARCH/printThePatternBinaryNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+cout<<"enter the value of n = ";
+cin>>n;
+    for(i=1; i<=n; i++){
+        for(j=1; j<=i; j++){
+        if(i%2!=0 && j%2!=0){
+        cout<<j;
+        }
+        else{
+            cout<<j;
+        }
+        }
+        cout<<endl;
+    }
+}
