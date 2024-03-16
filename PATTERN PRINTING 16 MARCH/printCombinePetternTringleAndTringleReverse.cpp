539824688ced50ecbc62125405e3c889241e1bfe
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-16 15:29:55 +0530

    print the combine pettern tringle and reverse tringle

---
+++ PATTERN PRINTING 16 MARCH/printCombinePetternTringleAndTringleReverse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter the n = ";
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+     for(i=1; i<=(n-1); i++){
+        for(j=i; j<=(n-1); j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+   
+}
