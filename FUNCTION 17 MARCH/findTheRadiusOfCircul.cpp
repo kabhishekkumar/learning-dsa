8430a36ece3737da0e0091f7ad85f82bb09f73ba
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-17 18:01:46 +0530

    print the area of circle the helps of function

---
+++ FUNCTION 17 MARCH/findTheRadiusOfCircul.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+    int area(int r){
+     return 3.14*r*r;
+
+    }
+    int main(){
+        int r;
+
+        cout<<"enter the r = ";
+        cin>>r;
+        int value = area(r);
+        cout<<"print the area of circule = "<<value;
+
+}
