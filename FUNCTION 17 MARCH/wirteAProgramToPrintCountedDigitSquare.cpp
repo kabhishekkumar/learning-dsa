8ee3f5c64eec495841812e2e3170055643256f51
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-18 15:36:28 +0530

    print the square of all counted digit by using function

---
+++ FUNCTION 17 MARCH/wirteAProgramToPrintCountedDigitSquare.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int square(int s){
+    return s*s;
+}
+int main(){
+    int number;
+    cout<<"enter the value of number = ";
+    cin>>number;
+    number = abs(number);
+  int count = 1;
+    while(number/=10){
+        count++;
+    }
+    cout<<"print all counted digit number = "<<count<<endl;
+
+    int result= square(count);
+    cout<<"print the square of counted digit = "<<result<<endl;
+
+}
