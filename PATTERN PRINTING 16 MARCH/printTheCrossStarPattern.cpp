7a11ed13f201a13caaee02ad8244ee32f380d682
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-19 22:26:34 +0530

    print the cross star pattern

---
+++ PATTERN PRINTING 16 MARCH/printTheCrossStarPattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+cout<<"enter the value of n = ";
+cin>>n;
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+       if(i==j){
+        cout<<"*";
+       }
+       
+       if(i+j==6){
+        cout<<"*";
+       }
+       cout<<" ";
+        }
+        cout<<endl;
+        }
+}
