160b431ca77d9fd795ac93099a565aaf6f91b0f9
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-21 22:17:12 +0530

    print the reberse tringle pattern

--- PATTERN PRINTING 16 MARCH/printReverseTringleUsingNumber.cpp
+++ PATTERN PRINTING 16 MARCH/printReverseTringleUsingNumber.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int i,j,n;
-cout<<"enter the value of n = ";
+cout<<"enter the  value of no: = ";
 cin>>n;
     for(i=0; i<=n; i++){
         for(j=1; j<=(n-i); j++){
