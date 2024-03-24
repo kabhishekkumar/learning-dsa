5c51121a71f43f29550e2f44ba2f0d219f62d0c4
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-24 23:29:45 +0530

    print the  star cross pattern using loop

--- PATTERN PRINTING 16 MARCH/printTheCrossStarPattern.cpp
+++ PATTERN PRINTING 16 MARCH/printTheCrossStarPattern.cpp
@@ -6,13 +6,11 @@ cout<<"enter the value of number = ";
 cin>>n;
     for(i=1; i<=n; i++){
         for(j=1; j<=n; j++){
-       if(i==j){
+       if(i==j || (i+j)==(n+1)){
         cout<<"*";
        }
        
-       if(i+j==6){
-        cout<<"*";
-       }
+       
        cout<<" ";
         }
         cout<<endl;
