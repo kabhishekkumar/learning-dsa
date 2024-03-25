b60c4130600d57c40dde3b7a820e0efbace5e441
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-25 22:50:28 +0530

    print binary pattern

--- PATTERN PRINTING 16 MARCH/printTheBinaryTringlePattern.cpp
+++ PATTERN PRINTING 16 MARCH/printTheBinaryTringlePattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int i,j,n;
-cout<<"enter the value of n = ";
+cout<<"enter the value of number:  ";
 cin>>n;
     for(i=1; i<=n; i++){
         for(j=1; j<=i; j++){
