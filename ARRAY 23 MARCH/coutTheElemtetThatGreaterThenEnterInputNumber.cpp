5d53b786405c6cdcd8048ff436d6968a7d469af2
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-30 00:33:37 +0530

    count the greater number than enter by user

---
+++ ARRAY 23 MARCH/coutTheElemtetThatGreaterThenEnterInputNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    int count=0;
+    int a[100];
+    int item;
+    cout<<"enter the item = ";
+    cin>>item;
+    cout<<"enter the value of n = ";
+    cin>>n;
+
+    cout<<"enter the element of array = ";
+    for(i=0; i<n; i++){
+        cin>>a[i];
+    }
+    
+ for(i=1; i<n; i++){
+       if(item<a[i]){
+   count++;
+       }
+    }
+    cout<<"these some value of array is grater than item ="<<count<<endl;
+    
+    return 0;
+}
