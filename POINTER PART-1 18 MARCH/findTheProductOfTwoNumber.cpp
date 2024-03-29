0701e25ea2dafe1d5abe8a4c7c625a1312788a81
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-29 23:14:23 +0530

    print the product of two number using pointer

---
+++ POINTER PART-1 18 MARCH/findTheProductOfTwoNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int num1,num2,num3;
+   int *ptr1, *ptr2, *ptr3;
+cout<<"enter the value of num1= ";
+cin>>num1;
+cout<<"enter the value of num2= ";
+cin>>num2;
+
+ptr1 = &num1;
+ptr2 = &num2;
+ptr3 = &num3;
+
+*ptr3 = (*ptr1) * (*ptr2);
+cout<<"print the product of two number = "<<*ptr3;
+
+}
