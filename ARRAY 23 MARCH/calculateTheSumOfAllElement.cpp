117c35c7c7e3048c3f93f5cc74d53eebdbe59b7f
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-29 23:41:21 +0530

    print the sum of all array element array

---
+++ ARRAY 23 MARCH/calculateTheSumOfAllElement.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    int sum=0;
+    int a[100];
+    cout<<"enter the value of n = ";
+    cin>>n;
+     
+     cout<<"enter the element of array = ";
+    for(i=0; i<=n; i++){
+        cin>>a[i];
+    }
+
+    for(i=0; i<=n; i++){
+        sum = sum+ a[i];
+       
+    }
+     cout<<"print the sum of all element array = "<<sum;
+}
