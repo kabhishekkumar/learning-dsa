2de1ffe0d13004eedbd66b41fac0fae773644845
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-30 00:17:09 +0530

    find the maximum value of this array

---
+++ ARRAY 23 MARCH/findTheMaximumValueOfArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    int pre;
+    int a[100];
+    
+    cout<<"enter the value of n = ";
+    cin>>n;
+
+    cout<<"enter the element of array = ";
+    for(i=0; i<n; i++){
+        cin>>a[i];
+    }
+    pre = a[0];
+ for(i=1; i<n; i++){
+       if(pre<a[i]){
+      pre= a[i];
+       }
+    }
+    cout<<"this largest elemet of this array"<<pre;
+    return 0;
+}
