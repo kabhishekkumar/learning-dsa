bf3a55da93938a337b482bdec869d53df9cb8d1e
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-03-29 23:54:53 +0530

    find the elemnt of array

---
+++ ARRAY 23 MARCH/searchTheElementOfArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    int item;
+    int a[100];
+    cout<<"enter the item = ";
+    cin>>item;
+    cout<<"enter the value of n = ";
+    cin>>n;
+
+    cout<<"enter the element of array = ";
+    for(i=0; i<n; i++){
+        cin>>a[i];
+
+    }
+ for(i=0; i<n; i++){
+       if(a[i]==item){
+cout<<"item is find this index = "<<i<<endl;
+       }
+      
+
+    }
+    
+}
