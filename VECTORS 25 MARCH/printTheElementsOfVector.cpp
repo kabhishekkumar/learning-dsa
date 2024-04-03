c09fa9d4771f816e9967b98d0499e5f0bfe18baa
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-03 23:05:33 +0530

    print the element of vectors and the insert the element and delete the element

--- VECTORS 25 MARCH/printTheElementsOfVector.cpp
+++ VECTORS 25 MARCH/printTheElementsOfVector.cpp
@@ -13,11 +13,20 @@ for(int i=0; i<n; i++){
     cin>>element;
     v.push_back(element);
 }
+v.insert(v.begin()+3,8);
 
 for(int i=0; i<v.size(); i++){ //we use n  place of v.size()
 cout<<v[i]<<" ";
+
+
 }
+v.erase(v.end()-2);
     cout<<endl;
+   for(int i=0; i<v.size(); i++){ //we use n  place of v.size()
+cout<<v[i]<<" ";
+   }
+   cout<<endl;
+   
 }
 
    //or
