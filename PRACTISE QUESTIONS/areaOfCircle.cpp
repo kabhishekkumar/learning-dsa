2d4d13aea6b0a46a9712388968011b91fbc1e3ed
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-11 21:00:17 +0530

    find the area of circle

--- PRACTISE QUESTIONS/areaOfCircle.cpp
+++ PRACTISE QUESTIONS/areaOfCircle.cpp
@@ -1,8 +1,10 @@
+//find the area  of circle
+
 #include<iostream>
 using namespace std;
 int main(){
   float r,area;
-    cout<<"enter the radius of circule";
+    cout<<"enter the radius of circule:";
     cin>>r;
   area = 3.14*r*r;
     cout<<area;
