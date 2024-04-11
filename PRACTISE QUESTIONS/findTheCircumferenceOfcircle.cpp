c791e9e2cb4e78d2c1d4532f6a45f3faaa92349c
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-11 21:04:19 +0530

    find the circumference of circle

--- PRACTISE QUESTIONS/findTheCircumferenceOfcircle.cpp
+++ PRACTISE QUESTIONS/findTheCircumferenceOfcircle.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(){
     float r,circumference;
-    cout<<"enter the value of r";
+    cout<<"enter the value of radius:";
     cin>>r;
 
     circumference = 2*3.14*r;
     cout<<"find the circumference of circle = "<<circumference;
+    return 0;
 
 }
