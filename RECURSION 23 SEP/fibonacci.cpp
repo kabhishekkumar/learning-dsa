d1027731510235ca942a3caabfbbcf4875beea8d
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-09-23 22:44:47 +0530

    print fibonacci series using recursion

--- RECURSION 23 SEP/fibonacci.cpp
+++ RECURSION 23 SEP/fibonacci.cpp
@@ -1,13 +1,21 @@
 #include<iostream>  
 using namespace std;
-void fibo(int n){
-if(n==10) return;
-int fan = (n-1)+(n-2);
-cout<<fan<<endl;
-fibo(n+1);
+// void fibo(int n){
+// if(n==10) return;
+// int fan = (n-1)+(n-2);
+// cout<<fan<<endl;
+// fibo(n+1);
+// }
+
+// int main(){
+//     fibo(0);
+// }
+
+int fib(int n){
+    if(n==1 || n==2) return 1;
+    return fib(n-1)+fib(n-2);
 }
 
 int main(){
-    fibo(0);
+cout<<fib(10);
 }
-
